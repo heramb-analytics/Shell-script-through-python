fb6531878bc8e26c497eef6b5a2da0fcb6928bf4
heramb-analytics <93883195+heramb-analytics@users.noreply.github.com>
2021-12-14 18:16:48 +0530

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import subprocess
+subprocess.call(['sh', './test.sh'])
+
+
+# In[11]:
+
+
+import subprocess
+subprocess.run(["ls"])
+
+
+# In[ ]:
+
+
+
+
